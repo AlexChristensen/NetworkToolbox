3f5d22855af20d32c00093920880c0a57f8bb15a
AlexChristensen <32655669+AlexChristensen@users.noreply.github.com>
2017-12-04 20:46:25 -0500

    Add files via upload

--- R/NetworkToolbox--master.R
+++ R/NetworkToolbox--master.R
@@ -25,161 +25,149 @@
 #TMFG Filtering Method----
 TMFG <-function (data, binary = FALSE, weighted = TRUE, depend = FALSE)
 {
-  if(nrow(data)==ncol(data)){cormat<-data}else
-    if(binary){cormat<-psych::tetrachoric(data)$rho}else{cormat<-cor(data)}
-  n<-ncol(cormat)
-  if(n<9){print("Matrix is too small")}
-  nodeTO<-array()
-  nodeFROM<-array()
-  nodeWEIGHT<-array()
-  count<-0
-  for(i in 1:nrow(cormat))
-    for(j in 1:ncol(cormat))
-      if(cormat[i,j] != 0)
-      {
-        count<-count+1
-        nodeTO[count]<-i
-        nodeFROM[count]<-j
-        nodeWEIGHT[count]<-cormat[i,j]
-      }
-  M<-cbind(nodeTO,nodeFROM,nodeWEIGHT) #create node-weight matrix
-  in_v<-matrix(nrow=nrow(cormat),ncol=1) #initialize inserted vertices
-  ou_v<-matrix(nrow=nrow(cormat),ncol=1) #initialize not yet inserted vertices
-  tri<-matrix(nrow=((2*n)-4),ncol=3) #initializaes triangles
-  separators<-matrix(nrow=n-4,ncol=3)#initialize list of 3-cliques (non-face triangles)
-  #find 3 vertices with largest strength
-  #s<-colSums(cormat*(cormat>mean(matrix(cormat,nrow=1)))*1) ##old s
-  if(!depend){s<-colSums(cormat*(cormat>mean(matrix(unlist(cormat),nrow=1)))*1)
-  }else{s<-(colSums(cormat*(cormat>mean(matrix(unlist(cormat),nrow=1)))*1)
-            +rowSums(cormat*(cormat>mean(matrix(unlist(cormat),nrow=1)))*1))}
-  in_v[1:4]<-order(s,decreasing=TRUE)[1:4]
-  ou_v<-setdiff(1:nrow(in_v),in_v)
-  #build tetrahedron with the largest strength
-  tri[1,]<-in_v[1:3,]
-  tri[2,]<-in_v[2:4,]
-  tri[3,]<-in_v[c(1,2,4),]
-  tri[4,]<-in_v[c(1,3,4),]
-  S<-matrix(nrow=(3*nrow(cormat)-6),ncol=3) #initialize sparse matrix
-  if(!depend){S[1,]<-c(in_v[1],in_v[2],1)
-  S[2,]<-c(in_v[1],in_v[3],1)
-  S[3,]<-c(in_v[1],in_v[4],1)
-  S[4,]<-c(in_v[2],in_v[3],1)
-  S[5,]<-c(in_v[2],in_v[4],1)
-  S[6,]<-c(in_v[3],in_v[4],1)
-  }else{if(cormat[in_v[1],in_v[2]]>cormat[in_v[2],in_v[1]])
-      {S[1,]<-c(in_v[1],in_v[2],1)
-      }else{S[1,]<-c(in_v[2],in_v[1],1)}
-      if(cormat[in_v[1],in_v[3]]>cormat[in_v[3],in_v[1]])
-      {S[2,]<-c(in_v[1],in_v[3],1)
-      }else{S[2,]<-c(in_v[3],in_v[1],1)}
-      if(cormat[in_v[1],in_v[4]]>cormat[in_v[4],in_v[1]])
-      {S[3,]<-c(in_v[1],in_v[4],1)
-      }else{S[3,]<-c(in_v[4],in_v[1],1)}
-      if(cormat[in_v[2],in_v[3]]>cormat[in_v[3],in_v[2]])
-      {S[4,]<-c(in_v[2],in_v[3],1)
-      }else{S[4,]<-c(in_v[3],in_v[2],1)}
-      if(cormat[in_v[2],in_v[4]]>cormat[in_v[4],in_v[2]])
-      {S[5,]<-c(in_v[2],in_v[4],1)
-      }else{S[5,]<-c(in_v[4],in_v[2],1)}
-      if(cormat[in_v[3],in_v[4]]>cormat[in_v[4],in_v[3]])
-      {S[6,]<-c(in_v[3],in_v[4],1)
-      }else{S[6,]<-c(in_v[4],in_v[3],1)}
-  }
-  #build initial gain table
-  gain<-matrix(-Inf,nrow=n,ncol=(2*(n-2)))
-  if(!depend){gain[ou_v,1]<-rowSums(cormat[ou_v,(tri[1,])])
-  gain[ou_v,2]<-rowSums(cormat[ou_v,(tri[2,])])
-  gain[ou_v,3]<-rowSums(cormat[ou_v,(tri[3,])])
-  gain[ou_v,4]<-rowSums(cormat[ou_v,(tri[4,])])
-  }else{
-      gain[ou_v,1]<-rowSums(cormat[ou_v,(tri[1,])])+colSums(cormat[(tri[1,]),ou_v])
-      gain[ou_v,2]<-rowSums(cormat[ou_v,(tri[2,])])+colSums(cormat[(tri[2,]),ou_v])
-      gain[ou_v,3]<-rowSums(cormat[ou_v,(tri[3,])])+colSums(cormat[(tri[3,]),ou_v])
-      gain[ou_v,4]<-rowSums(cormat[ou_v,(tri[4,])])+colSums(cormat[(tri[4,]),ou_v])
-  }
-  ntri<-4 #number of triangles
-  gij<-matrix(nrow=1,ncol=272)
-  v<-matrix(nrow=1,ncol=272)
-  ve<-array()
-  tr<-0
-  for(e in 5:n)
-  {
-    if(length(ou_v)==1){
-      ve<-ou_v
-      v<-1
-      w<-1
-      tr<-which.max(gain[ou_v,])
-    }else{
-      for(q in 1:ncol(gain))
-      {
-        gij[,q]<-max(gain[ou_v,q])
-        v[,q]<-which.max(gain[ou_v,q])
-        tr<-which.max(gij)
-      }
-      ve<-ou_v[v[tr]]
-      w<-v[tr]
+    if(nrow(data)==ncol(data)){cormat<-data}else
+        if(binary){cormat<-psych::tetrachoric(data)$rho}else{cormat<-cor(data)}
+    n<-ncol(cormat)
+    if(n<9){print("Matrix is too small")}
+    nodeTO<-array()
+    nodeFROM<-array()
+    nodeWEIGHT<-array()
+    count<-0
+    for(i in 1:nrow(cormat))
+        for(j in 1:ncol(cormat))
+            if(cormat[i,j] != 0)
+            {
+                count<-count+1
+                nodeTO[count]<-i
+                nodeFROM[count]<-j
+                nodeWEIGHT[count]<-cormat[i,j]
+            }
+    M<-cbind(nodeTO,nodeFROM,nodeWEIGHT) #create node-weight matrix
+    in_v<-matrix(nrow=nrow(cormat),ncol=1) #initialize inserted vertices
+    ou_v<-matrix(nrow=nrow(cormat),ncol=1) #initialize not yet inserted vertices
+    tri<-matrix(nrow=((2*n)-4),ncol=3) #initializaes triangles
+    separators<-matrix(nrow=n-4,ncol=3)#initialize list of 3-cliques (non-face triangles)
+    #find 3 vertices with largest strength
+    #s<-colSums(cormat*(cormat>mean(matrix(cormat,nrow=1)))*1) ##old s
+    s<-rowSums(cormat*(cormat>mean(matrix(unlist(cormat),nrow=1)))*1)
+    in_v[1:4]<-order(s,decreasing=TRUE)[1:4]
+    ou_v<-setdiff(1:nrow(in_v),in_v)
+    #build tetrahedron with the largest strength
+    tri[1,]<-in_v[1:3,]
+    tri[2,]<-in_v[2:4,]
+    tri[3,]<-in_v[c(1,2,4),]
+    tri[4,]<-in_v[c(1,3,4),]
+    S<-matrix(nrow=(3*nrow(cormat)-6),ncol=3) #initialize sparse matrix
+    if(!depend){S[1,]<-c(in_v[1],in_v[2],1)
+    S[2,]<-c(in_v[1],in_v[3],1)
+    S[3,]<-c(in_v[1],in_v[4],1)
+    S[4,]<-c(in_v[2],in_v[3],1)
+    S[5,]<-c(in_v[2],in_v[4],1)
+    S[6,]<-c(in_v[3],in_v[4],1)
+    }else{if(cormat[in_v[1],in_v[2]]>cormat[in_v[2],in_v[1]])
+    {S[1,]<-c(in_v[1],in_v[2],1)
+    }else{S[1,]<-c(in_v[2],in_v[1],1)}
+        if(cormat[in_v[1],in_v[3]]>cormat[in_v[3],in_v[1]])
+        {S[2,]<-c(in_v[1],in_v[3],1)
+        }else{S[2,]<-c(in_v[3],in_v[1],1)}
+        if(cormat[in_v[1],in_v[4]]>cormat[in_v[4],in_v[1]])
+        {S[3,]<-c(in_v[1],in_v[4],1)
+        }else{S[3,]<-c(in_v[4],in_v[1],1)}
+        if(cormat[in_v[2],in_v[3]]>cormat[in_v[3],in_v[2]])
+        {S[4,]<-c(in_v[2],in_v[3],1)
+        }else{S[4,]<-c(in_v[3],in_v[2],1)}
+        if(cormat[in_v[2],in_v[4]]>cormat[in_v[4],in_v[2]])
+        {S[5,]<-c(in_v[2],in_v[4],1)
+        }else{S[5,]<-c(in_v[4],in_v[2],1)}
+        if(cormat[in_v[3],in_v[4]]>cormat[in_v[4],in_v[3]])
+        {S[6,]<-c(in_v[3],in_v[4],1)
+        }else{S[6,]<-c(in_v[4],in_v[3],1)}
     }
-    #update vertex lists
-    ou_v<-ou_v[-w]
-    in_v[e]<-ve
-    #update adjacency matrix
-    for(u in 1:length(tri[tr,]))
+    #build initial gain table
+    gain<-matrix(-Inf,nrow=n,ncol=(2*(n-2)))
+    gain[ou_v,1]<-rowSums(cormat[ou_v,(tri[1,])])
+    gain[ou_v,2]<-rowSums(cormat[ou_v,(tri[2,])])
+    gain[ou_v,3]<-rowSums(cormat[ou_v,(tri[3,])])
+    gain[ou_v,4]<-rowSums(cormat[ou_v,(tri[4,])])
+    
+    ntri<-4 #number of triangles
+    gij<-matrix(nrow=1,ncol=272)
+    v<-matrix(nrow=1,ncol=272)
+    ve<-array()
+    tr<-0
+    for(e in 5:n)
     {
-      cou<-6+((3*(e-5))+u)
-      if(depend){
-      if(cormat[ve,tri[tr,u]]>cormat[tri[tr,u],ve]){
-          S[cou,]<-cbind(ve,tri[tr,u],1)   
-      }else{S[cou,]<-cbind(tri[tr,u],ve,1)}}else
-      S[cou,]<-cbind(ve,tri[tr,u],1)
+        if(length(ou_v)==1){
+            ve<-ou_v
+            v<-1
+            w<-1
+            tr<-which.max(gain[ou_v,])
+        }else{
+            for(q in 1:ncol(gain))
+            {
+                gij[,q]<-max(gain[ou_v,q])
+                v[,q]<-which.max(gain[ou_v,q])
+                tr<-which.max(gij)
+            }
+            ve<-ou_v[v[tr]]
+            w<-v[tr]
+        }
+        #update vertex lists
+        ou_v<-ou_v[-w]
+        in_v[e]<-ve
+        #update adjacency matrix
+        for(u in 1:length(tri[tr,]))
+        {
+            cou<-6+((3*(e-5))+u)
+            if(depend){
+                if(cormat[ve,tri[tr,u]]>cormat[tri[tr,u],ve]){
+                    S[cou,]<-cbind(ve,tri[tr,u],1)   
+                }else{S[cou,]<-cbind(tri[tr,u],ve,1)}}else
+                    S[cou,]<-cbind(ve,tri[tr,u],1)
+        }
+        #update 3-clique list
+        separators[e-4,]<-tri[tr,]
+        #update triangle list replacing 1 and adding 2 triangles
+        tri[ntri+1,]<-cbind(rbind(tri[tr,c(1,3)]),ve)
+        tri[ntri+2,]<-cbind(rbind(tri[tr,c(2,3)]),ve)
+        tri[tr,]<-cbind(rbind(tri[tr,c(1,2)]),ve)
+        #update gain table
+        gain[ve,]<-0
+            gain[ou_v,tr]<-rowSums(cormat[ou_v,tri[tr,],drop=FALSE])
+            gain[ou_v,ntri+1]<-rowSums(cormat[ou_v,tri[ntri+1,],drop=FALSE])
+            gain[ou_v,ntri+2]<-rowSums(cormat[ou_v,tri[ntri+2,],drop=FALSE])
+            
+        #update triangles
+        ntri<-ntri+2
     }
-    #update 3-clique list
-    separators[e-4,]<-tri[tr,]
-    #update triangle list replacing 1 and adding 2 triangles
-    tri[ntri+1,]<-cbind(rbind(tri[tr,c(1,3)]),ve)
-    tri[ntri+2,]<-cbind(rbind(tri[tr,c(2,3)]),ve)
-    tri[tr,]<-cbind(rbind(tri[tr,c(1,2)]),ve)
-    #update gain table
-    gain[ve,]<-0
-    if(!depend){
-    gain[ou_v,tr]<-rowSums(cormat[ou_v,tri[tr,],drop=FALSE])
-    gain[ou_v,ntri+1]<-rowSums(cormat[ou_v,tri[ntri+1,],drop=FALSE])
-    gain[ou_v,ntri+2]<-rowSums(cormat[ou_v,tri[ntri+2,],drop=FALSE])
+    L<-S
+    if(depend)
+    {W<-matrix(1:nrow(cormat),nrow=nrow(cormat),ncol=1)
+    X<-matrix(1:nrow(cormat),nrow=nrow(cormat),ncol=1)
+    Y<-matrix(0,nrow=nrow(cormat),ncol=1)
+    Z<-cbind(W,X,Y)
+    K<-rbind(L,Z)
     }else{
-        gain[ou_v,tr]<-rowSums(cormat[ou_v,tri[tr,],drop=FALSE])+colSums(cormat[tri[tr,],ou_v,drop=FALSE])
-        gain[ou_v,ntri+1]<-rowSums(cormat[ou_v,tri[ntri+1,],drop=FALSE])+colSums(cormat[tri[ntri+1,],ou_v,drop=FALSE])
-        gain[ou_v,ntri+2]<-rowSums(cormat[ou_v,tri[ntri+2,],drop=FALSE])+colSums(cormat[tri[ntri+1,],ou_v,drop=FALSE])
+        L[,1]<-S[,2]
+        L[,2]<-S[,1]
+        K<-rbind(S,L)
     }
-    #update triangles
-    ntri<-ntri+2
-  }
-  L<-S
-  if(depend)
-  {W<-matrix(1:nrow(cormat),nrow=nrow(cormat),ncol=1)
-  X<-matrix(1:nrow(cormat),nrow=nrow(cormat),ncol=1)
-  Y<-matrix(0,nrow=nrow(cormat),ncol=1)
-  Z<-cbind(W,X,Y)
-  K<-rbind(L,Z)
-  }else{
-  L[,1]<-S[,2]
-  L[,2]<-S[,1]
-  K<-rbind(S,L)
-  }
- 
-  x<-as.matrix(Matrix::sparseMatrix(i=K[,1],j=K[,2],x=K[,3]))
-  diag(x)<-0
-  
-  if(weighted)
-   {
-    for(r in 1:nrow(x))
-      for(z in 1:ncol(x))
-      {if(x[r,z]==1){x[r,z]<-cormat[r,z]}
-   }
-  }
-
-  x<-as.matrix(x)
-  colnames(x)<-colnames(cormat)
-  rownames(x)<-colnames(cormat)
-  return(list(A=x, sep=separators))
+    
+    x<-as.matrix(Matrix::sparseMatrix(i=K[,1],j=K[,2],x=K[,3]))
+    diag(x)<-0
+    
+    if(weighted)
+    {
+        for(r in 1:nrow(x))
+            for(z in 1:ncol(x))
+            {if(x[r,z]==1){x[r,z]<-cormat[r,z]}
+            }
+    }
+    
+    x<-as.matrix(x)
+    colnames(x)<-colnames(cormat)
+    rownames(x)<-colnames(cormat)
+    return(list(A=x, sep=separators))
 }
 #----
 #' Maximum Spanning Tree
