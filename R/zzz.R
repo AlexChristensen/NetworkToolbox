1eb11095a339d14ec929bd6480e0cc5fb9662e9a
Alexander Christensen <32655669+AlexChristensen@users.noreply.github.com>
2019-04-30 20:53:00 -0400

    package information on load

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+.onload <- function(libname, pkgname)
+{library.dynam("NetworkToolbox",package=pkgname,lib.loc=libname)}
+
+.onAttach <- function(libname, pkgname)
+{
+    temp <- packageDescription("NetworkToolbox")
+    msg <- paste("Package: ",temp$Package,": ",temp$Title,"\n",
+               "Version: ",temp$Version,"\n",
+               "Created on: ",
+               temp$Date,"\n", sep="")
+    msg <- paste(msg,"Author: Alexander P. Christensen, University of North Carolina at Greensboro\n",sep="")
+    msg <- paste(msg,'For citation information, type citation("NetworkToolbox").\n')
+    msg <- paste(msg,"For vignettes, see <https://doi.org/10.32614/RJ-2018-065> \n")
+    msg <- paste(msg,"For bugs and errors, submit an issue to <https://github.com/AlexChristensen/NetworkToolbox/issues>")
+    packageStartupMessage(msg)
+}
