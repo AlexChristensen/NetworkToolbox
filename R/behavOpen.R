d41da6d7bbf6165d358ebf55470531c6c876c18c
Alexander Christensen <alexpaulchristensen@gmail.com>
2025-05-08 10:54:22 -0500

    URL update

--- R/behavOpen.R
+++ R/behavOpen.R
@@ -2,31 +2,34 @@
 #'NEO-PI-3 for Resting-state Data
 #'
 #' NEO-PI-3 Openness to Experience associated with resting-state data (\emph{n} = 144).
-#' 
+#'
 #' Behavioral data of NEO-PI-3 associated with each connectivity matrix (open).
-#' 
-#' To access the resting-state brain data, please go to \url{https://drive.google.com/file/d/1ugwi7nRrlHQYuGPzEB4wYzsizFrIMvKR/view}
-#' 
+#'
+#' To access the resting-state brain data, please go to \url{https://drive.google.com/file/d/1T7_mComB6HPxJxZZwwsLLSYHXsOuvOBt/view?usp=sharing}
+#'
+#'
+#'
+#'
 #' @name behavOpen
-#' 
+#'
 #' @docType data
-#' 
+#'
 #' @usage data(behavOpen)
-#' 
+#'
 #' @format behavOpen (vector, length = 144)
-#' 
+#'
 #' @keywords datasets
-#' 
+#'
 #' @references
 #' Beaty, R. E., Chen, Q., Christensen, A. P., Qiu, J., Silvia, P. J., & Schacter, D. L. (2018).
 #' Brain networks of the imaginative mind: Dynamic functional connectivity of default and cognitive control networks relates to Openness to Experience.
 #' \emph{Human Brain Mapping}, \emph{39}, 811-821.
-#' 
+#'
 #' Beaty, R. E., Kenett, Y. N., Christensen, A. P., Rosenberg, M. D., Benedek, M., Chen, Q., ... & Silvia, P. J. (2018).
 #' Robust prediction of individual creative ability from brain functional connectivity.
 #' \emph{Proceedings of the National Academy of Sciences}, 201713532.
-#' 
-#' @examples 
+#'
+#' @examples
 #' data("behavOpen")
 NULL
 #----
